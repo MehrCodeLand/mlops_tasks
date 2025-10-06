356ca76290bc9ee461b63d4fed0f58d1b4a95609
mehrshad <mehrcodeland@gmail.com>
2025-10-06 11:36:21 +0330

    update - ex3

---
+++ ex_3/test.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from ex_3.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    res = client.get("/")
+    assert res.status_code == 200
+    assert res.json() == {"message": "Hello, World!"}
